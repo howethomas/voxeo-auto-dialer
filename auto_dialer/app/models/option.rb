4ea4959da0bd116026fe27b1abd65e5908d70b34
Thomas Howe <howethomas@aol.com>
2008-08-01 17:55:28 -0400

    Fixed options startup bug

--- auto_dialer/app/models/option.rb
+++ auto_dialer/app/models/option.rb
@@ -1,6 +1,6 @@
 class Option < ActiveRecord::Base
 
   belongs_to :account
-  validates_presence_of :debug_level, :admin_name, :admin_email, :admin_phone
+  validates_presence_of :debug_level
   validates_numericality_of :debug_level
 end
